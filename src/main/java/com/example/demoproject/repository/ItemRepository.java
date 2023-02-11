69792b09d8854b36d91042ffd2ed00d5c90f9600
Kirill <pub@gnumbix.com>
2023-02-11 16:51:45 +0200

    Item order reservation

--- src/main/java/com/example/demoproject/repository/ItemRepository.java
+++ src/main/java/com/example/demoproject/repository/ItemRepository.java
@@ -1,8 +1,14 @@
 package com.example.demoproject.repository;
 
 import com.example.demoproject.domain.Item;
+import com.example.demoproject.domain.enums.ItemStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Integer> {
+
+    List<Item> findAllByStatus(ItemStatus status, Pageable pageable);
 
 }
--- src/main/java/com/example/demoproject/service/OrderServiceImpl.java
+++ src/main/java/com/example/demoproject/service/OrderServiceImpl.java
@@ -1,9 +1,15 @@
 package com.example.demoproject.service;
 
+import com.example.demoproject.domain.Item;
 import com.example.demoproject.domain.Order;
+import com.example.demoproject.domain.enums.ItemStatus;
 import com.example.demoproject.model.OrderForm;
+import com.example.demoproject.repository.ItemRepository;
 import com.example.demoproject.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,8 +19,11 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
 
-    public OrderServiceImpl(OrderRepository orderRepository) {
+    private final ItemRepository itemRepository;
+
+    public OrderServiceImpl(OrderRepository orderRepository, ItemRepository itemRepository) {
         this.orderRepository = orderRepository;
+        this.itemRepository = itemRepository;
     }
 
     @Override
@@ -28,14 +37,28 @@ public class OrderServiceImpl implements OrderService {
     }
 
     @Override
+    @Transactional
     public Order add(OrderForm form) {
+
+        List<Item> itemsList = itemRepository.findAllByStatus(ItemStatus.ACTIVE, PageRequest.of(0, form.getQty()));
+
         Order order = new Order();
         order.setName(form.getName());
         order.setStatus(form.getStatus());
-        return this.orderRepository.save(order);
+        order.setItemsQuantity(itemsList.size());
+        order = this.orderRepository.save(order);
+
+        for(Item item : itemsList) {
+            item.setStatus(ItemStatus.RESERVED);
+            item.setOrderId(order.getId());
+            item.setOrder(order);
+        }
+
+        return order;
     }
 
     @Override
+    @Transactional
     public Order update(OrderForm form) {
         Optional<Order> orderOpt = this.findById(form.getId());
         if(orderOpt.isEmpty()) {
